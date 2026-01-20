aa2cf4fe470a4c257202b9dd75bac533f9757716
1dawd <1dawd@A204PC08.desarrolloweb.local>
2026-01-20 14:11:27 +0100

    Proyecto Tienda a 20/1/2026

---
+++ src/main/java/com/fhu86918/educastur/tienda/LineaPedido.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fhu86918.educastur.tienda;
+
+/**
+ *
+ * @author 1dawd
+ */
+public class LineaPedido {
+    private String idArticulo;
+    private int unidades;
+
+    public LineaPedido(String idArticulo, int unidades) {
+        this.idArticulo = idArticulo;
+        this.unidades = unidades;
+    }
+    
+    
+            
+}
---
+++ src/main/java/com/fhu86918/educastur/tienda/Tienda2026.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fhu86918.educastur.tienda;
+
+/**
+ *
+ * @author 1dawd
+ */
+public class Tienda2026 {
+
+    public static void main(String[] args) {
+        
+        
+    }
+        
+    
+    
+}
