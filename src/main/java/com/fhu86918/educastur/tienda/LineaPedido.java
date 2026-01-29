c465066da331fbbbe42c2c6b9167fe4c766464f2
1dawd <1dawd@A204PC08.desarrolloweb.local>
2026-01-29 14:23:03 +0100

    Proyecto Tienda2026  a 29/1/2026 - Get/Set de LineaPedido

--- src/main/java/com/fhu86918/educastur/tienda/LineaPedido.java
+++ src/main/java/com/fhu86918/educastur/tienda/LineaPedido.java
@@ -17,6 +17,24 @@ public class LineaPedido {
         this.unidades = unidades;
     }
 
+    public String getIdArticulo() {
+        return idArticulo;
+    }
+
+    public void setIdArticulo(String idArticulo) {
+        this.idArticulo = idArticulo;
+    }
+
+    public int getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(int unidades) {
+        this.unidades = unidades;
+    }
+    
+    
+
     @Override
     public String toString() {
         return "LineaPedido{" + "idArticulo=" + idArticulo + ", unidades=" + unidades + '}';
